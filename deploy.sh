b1c7a2ee96186006ca9374aa22d989eaeef907fc
msimonin <matthieu.simonin@inria.fr>
2016-04-04 11:29:39 +0200

    uncomment.
    M

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 vagrant up
-#vagrant ssh -c 'sudo /vagrant/01_prepare.sh &&\
-#  sudo /vagrant/01_vagrant.sh &&\
-#  sudo -i -u stack /vagrant/02_start.sh &&\
-#  /vagrant/03_rome.sh'
+vagrant ssh -c 'sudo /vagrant/01_prepare.sh &&\
+  sudo /vagrant/01_vagrant.sh &&\
+  sudo -i -u stack /vagrant/02_start.sh &&\
+  /vagrant/03_rome.sh'
 
-vagrant ssh -c '/vagrant/03_rome.sh'
