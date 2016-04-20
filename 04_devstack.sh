4f06cdf3bd1ea6885f439ff33171e6d684d6e9bd
Jonathan <jonathan.pastor@me.com>
2016-04-20 08:48:29 -0500

    generate local.conf in a 'template' fashion;

--- 04_devstack.sh
+++ 04_devstack.sh
@@ -6,19 +6,22 @@ set -x
 # on vagrant we can use the vagrant user
 
 cd /devstack
-echo '[[local|localrc]]' > local.conf
-echo ADMIN_PASSWORD=admin >> local.conf
-echo DATABASE_PASSWORD=admin >> local.conf
-echo RABBIT_PASSWORD=admin >> local.conf
-echo SERVICE_PASSWORD=admin >> local.conf
-echo HOST_IP=141.10.10.141 >> local.conf
-echo FLAT_INTERFACE=eth1 >> local.conf
-echo FLOATING_RANGE=141.10.10.196/26 >> local.conf
-echo FIXED_RANGE=10.11.12.0/24 >> local.conf
-echo FIXED_NETWORK_SIZE=256 >> local.conf
-echo NOVA_REPO=https://github.com/BeyondTheClouds/nova >> local.conf
-echo NOVA_BRANCH=disco/mitaka >> local.conf
-echo RECLONE=no >> local.conf
+
+cat > local.conf <<- EOM
+'[[local|localrc]]'
+ADMIN_PASSWORD=admin
+DATABASE_PASSWORD=admin
+RABBIT_PASSWORD=admin
+SERVICE_PASSWORD=admin
+HOST_IP=141.10.10.141
+FLAT_INTERFACE=eth1
+FLOATING_RANGE=141.10.10.196/26
+FIXED_RANGE=10.11.12.0/24
+FIXED_NETWORK_SIZE=256
+NOVA_REPO=https://github.com/BeyondTheClouds/nova
+NOVA_BRANCH=disco/mitaka
+RECLONE=no
+EOM
 
 sudo service redis-server stop
 sudo rm /var/lib/redis/dump.rdb
