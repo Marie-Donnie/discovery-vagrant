42596305f029551d4fbcb0a506342b28cc58595a
Marie-Donnie <aldanna.riel@gmail.com>
2016-06-09 15:18:02 +0200

    Changes for quicker deployment

--- 05_devstack.sh
+++ 05_devstack.sh
@@ -18,10 +18,13 @@ FLAT_INTERFACE=eth1
 FLOATING_RANGE=141.10.10.196/26
 FIXED_RANGE=10.11.12.0/24
 FIXED_NETWORK_SIZE=256
+GIT_DEPTH=1
 NOVA_REPO=https://github.com/BeyondTheClouds/nova
 NOVA_BRANCH=disco/mitaka
 RECLONE=no
 EOM
+
+sed -i 's/git_timed clone $git_clone_flags $git_remote $git_dest$/& -b $git_ref/' functions-common
 
 sudo service redis-server stop
 sudo rm /var/lib/redis/dump.rdb
