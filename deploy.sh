a4939fdbc7770aba4c8bc8eca4f8d09be4346b16
Marie-Donnie <delavergnemarie@gmail.com>
2020-06-17 10:15:14 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-vagrant up pop0
-vagrant ssh pop0 -c 'sudo /vagrant/01_vagrant.sh'
-vagrant ssh pop0 -c 'sudo /vagrant/02_rome.sh'
-vagrant ssh pop0 -c 'sudo /vagrant/03_redis.sh'
-vagrant ssh pop0 -c 'sudo /vagrant/04_prepare_devstack.sh'
-vagrant ssh pop0 -c 'sudo -i -u stack /vagrant/05_devstack.sh'
-vagrant ssh pop0 -c 'sudo /vagrant/06_nfs-server.sh'
-
+#sudo /vagrant/01_vagrant.sh
+sudo /vagrant/02_rome.sh
+sudo /vagrant/03_redis.sh
+sudo /vagrant/04_prepare_devstack.sh
+sudo -i -u stack /vagrant/05_devstack.sh
+sudo /vagrant/06_nfs-server.sh
