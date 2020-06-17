b96313dd7d88a23dd2350a1b1366e878d96dac35
Marie-Donnie <delavergnemarie@gmail.com>
2020-06-17 10:28:07 +0200

    removed folder

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 #sudo /vagrant/01_vagrant.sh
-sudo /vagrant/02_rome.sh
-sudo /vagrant/03_redis.sh
-sudo /vagrant/04_prepare_devstack.sh
-sudo -i -u stack /vagrant/05_devstack.sh
-sudo /vagrant/06_nfs-server.sh
+sudo 02_rome.sh
+sudo 03_redis.sh
+sudo 04_prepare_devstack.sh
+sudo -i -u stack 05_devstack.sh
+sudo 06_nfs-server.sh
